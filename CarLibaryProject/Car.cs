9b30ff2d11b6878c16aeea5ffb3da8e71a3b383e
Rex77pro <smaurieka@gmail.com>
2023-02-13 17:21:58 +0100

    Add project files.

---
+++ CarLibaryProject/Car.cs
@@ -0,0 +1,33 @@
+﻿namespace CarLibaryProject
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public string LicensePlate { get; set; }
+
+        public void ModelValidator()
+        {
+            if (Model.Length < 4)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        public void PriceValidator()
+        {
+            if (Price < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        public void LPValidator()
+        {
+            if (LicensePlate.Length <= 2 || LicensePlate.Length > 7)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+    }
+}
---
+++ CarLibaryProjectTests/CarTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarLibaryProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibaryProject.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        // Accepted Cars
+        private Car goodCar =       new Car { Id = 1, Model = "718 Boxster",    Price = 1099900,    LicensePlate = "XA55018" };
+
+        // Fail Cars
+        private Car FailModelCar =  new Car { Id = 3, Model = "",               Price = 200000,     LicensePlate = "AA12345" };
+        private Car FailPriceCar =  new Car { Id = 4, Model = "Passat",         Price = -2,         LicensePlate = "AA12345" };
+        private Car FailLPCar =     new Car { Id = 5, Model = "Passat",         Price = 200000,     LicensePlate = "" };
+
+        [TestMethod()]
+        public void ModelValidatorTest()
+        {
+            goodCar.ModelValidator();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => FailModelCar.ModelValidator());
+        }
+
+        [TestMethod()]
+        public void PriceValidatorTest()
+        {
+            goodCar.PriceValidator();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => FailPriceCar.PriceValidator());
+        }
+
+        [TestMethod()]
+        public void LPValidatorTest()
+        {
+            goodCar.LPValidator();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => FailLPCar.LPValidator());
+        }
+    }
+}
